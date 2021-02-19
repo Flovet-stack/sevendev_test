64012e3ebf4cfcf80e159b9c2c61b59d7ebae0ff
Flovet-stack <flovetoloveto@yahoo.com>
2021-02-19 01:02:10 +0100

    g
    Signed-off-by: Flovet-stack <flovetoloveto@yahoo.com>

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -2,6 +2,7 @@ const loaderBG = document.createElement('div');
 const mainBody = document.querySelector('html');
 loaderBG.classList.add('loader-bg')
 
+console.log(mainBody)
 document.addEventListener('readystatechange', () => {
     console.log(document.readyState)
 });
